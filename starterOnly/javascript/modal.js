ec3e837d4fac0f621e308a520c93103dd1acae29
Desbonnets <gillesdesbonnets@gmail.com>
2022-11-25 17:24:52 +0100

    fix js

--- starterOnly/javascript/modal.js
+++ starterOnly/javascript/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  let nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
   } else {
-    x.className = "topnav";
+    nav.className = "topnav";
   }
 }
 
